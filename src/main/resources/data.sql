25d9d9126b952698ccaf0c829f1dd9befdda356d
enis <enis_akyol@yahoo.com>
2023-11-06 10:12:21 +0300

    Spring Data Entity and Id Annotations

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into EMPLOYEES (id, name)
+values (1, 'Mike');
+
+insert into Student(id, first_name, last_name, email)
+values (1, 'Jules', 'Fules', 'jf@gmaikl.com');
+
+insert into Student(id, first_name, last_name, email)
+values (2, 'Mitch', 'Dirk', 'md@gmaikl.com');
+
